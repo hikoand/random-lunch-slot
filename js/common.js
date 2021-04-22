e2899f88e3f5d26be5086c6330b0569c2b8a672b
hikoand <dorongking2@gmail.com>
2021-04-22 21:34:13 +0900

    main-text-update
    text space update

--- js/common.js
+++ js/common.js
@@ -1,23 +1,21 @@
 //런치리스트 배열생성
-let lunchList = ["떡볶이", "우동", "햄버거", "냉면", "마라탕"]
+let lunchList = ["떡볶이", "우동", "햄버거", "냉면", "마라탕"];
 
 //배열복사
 let firstLunchList = [];
 
-lunchList.forEach(function(item) {
+lunchList.forEach(function (item) {
   firstLunchList.push(item);
 });
 
 let distanceList = ["10m", "8m", "7m", "7m", "9m"];
 let costList = ["7000won", "5000won", "4500won", "8000won", "9000won"];
 
-
 //제어할 요소선택 후 변수에 담기
 let displaySlot = document.querySelector(".menu_slot"); //menu slot
 let elem = document.querySelector(".menu_print > h2"); //menu print
 let distanceTxt = document.querySelector(".main_text> p > span"); //distance
 let costTxt = document.querySelector("em"); //cost
-
 
 //reset check
 let resetNum = 1;
@@ -50,8 +48,8 @@ function lunchIs() {
 
     //선택된 메뉴의 indexOf를 이용하여 distanceTxt, costTxt 노출
     let i = firstLunchList.indexOf(lunckPick);
-    distanceTxt.innerHTML = distanceList[i];
-    costTxt.innerHTML = costList[i];
+    distanceTxt.innerHTML = `:${distanceList[i]}`;
+    costTxt.innerHTML = `:${costList[i]}`;
 
     //reset 되었을 경우에 숨겨진 메뉴를 다시 노출시킴
     if (resetNum == 0) {
@@ -69,8 +67,8 @@ function reset() {
   displaySlot.style.display = "block";
 
   //distance, cost 초기화
-  distanceTxt.innerHTML = "??m";
-  costTxt.innerHTML = "?000won";
+  distanceTxt.innerHTML = ":??m";
+  costTxt.innerHTML = ":?000won";
 
   //resetNum으로 reset여부를 구분하기 위해 0 할당
   resetNum = 0;
