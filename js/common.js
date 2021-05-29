de4818577da73519663967a8e36b2034dc82b745
hikoand <71857799+hikoand@users.noreply.github.com>
2021-05-29 21:47:47 +0900

    JS: button disabled

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 //런치리스트 배열생성
-let lunchList = ["떡볶이", "우동", "햄버거", "냉면", "마라탕"];
+let lunchList = ['떡볶이', '우동', '햄버거', '냉면', '마라탕'];
 
 //배열복사
 let firstLunchList = [];
@@ -8,20 +8,23 @@ lunchList.forEach(function (item) {
   firstLunchList.push(item);
 });
 
-let distanceList = ["10m", "8m", "7m", "7m", "9m"];
-let costList = ["7000won", "5000won", "4500won", "8000won", "9000won"];
+let distanceList = ['10m', '8m', '7m', '7m', '9m'];
+let costList = ['7000won', '5000won', '4500won', '8000won', '9000won'];
 
 //제어할 요소선택 후 변수에 담기
-let displaySlot = document.querySelector(".menu_slot"); //menu slot
-let elem = document.querySelector(".menu_print > h2"); //menu print
-let distanceTxt = document.querySelector(".main_text> p > span"); //distance
-let costTxt = document.querySelector("em"); //cost
+let displaySlot = document.querySelector('.menu_slot'); //menu slot
+let elem = document.querySelector('.menu_print > h2'); //menu print
+let distanceTxt = document.querySelector('.main_text> p > span'); //distance
+let costTxt = document.querySelector('em'); //cost
+let btn = document.querySelector('.btn_area > button');
 
 //reset check
 let resetNum = 1;
 
 //LunchIs 함수선언
 function lunchIs() {
+  btn.disabled = true;
+
   //setTimeout 선언
   setTimeout(timeFunc, 900);
 
@@ -36,14 +39,14 @@ function lunchIs() {
     }
 
     //슬롯애니메이션 감추기
-    displaySlot.style.display = "none";
+    displaySlot.style.display = 'none';
 
-    //shuffle 메소드를 사용하여 석은 배열에서 index[0]을 가져오기
-    console.log(shuffle(lunchList));
+    //shuffle 메소드를 사용하여 섞은 배열에서 index[0]을 가져오기
+    // console.log(shuffle(lunchList));
     let lunckPick = shuffle(lunchList)[0];
 
     //메뉴 노출
-    console.log(lunckPick);
+    // console.log(lunckPick);
     elem.innerHTML = lunckPick;
 
     //선택된 메뉴의 indexOf를 이용하여 distanceTxt, costTxt 노출
@@ -53,22 +56,24 @@ function lunchIs() {
 
     //reset 되었을 경우에 숨겨진 메뉴를 다시 노출시킴
     if (resetNum == 0) {
-      elem.style.display = "block";
+      elem.style.display = 'block';
     }
   }
 }
 
 //reset 함수선언
 function reset() {
+  btn.disabled = false;
+
   //메뉴 숨기기
-  elem.style.display = "none";
+  elem.style.display = 'none';
 
   //슬롯애니메이션 노출
-  displaySlot.style.display = "block";
+  displaySlot.style.display = 'block';
 
   //distance, cost 초기화
-  distanceTxt.innerHTML = ":??m";
-  costTxt.innerHTML = ":?000won";
+  distanceTxt.innerHTML = ':??m';
+  costTxt.innerHTML = ':?000won';
 
   //resetNum으로 reset여부를 구분하기 위해 0 할당
   resetNum = 0;
